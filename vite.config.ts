754fd71198b1ad5ed54a9de57debbe57899ccbc5
Dave Addison <djaddison.vcs@gmail.com>
2022-07-31 18:46:24 -0400

    Moved index.html to src/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { join } from "path"
+
+const PROJECT_ROOT = process.cwd()
+const projectPath = (path: string) => join(PROJECT_ROOT, path)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  root: projectPath("src"),
+  build: {
+    outDir: projectPath("dist"),
+    emptyOutDir: true,
+  },
 })
