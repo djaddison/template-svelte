59a8b2a273f1ef0e8a1ff000ed51c1b1f82f238c
Dave Addison <djaddison.vcs@gmail.com>
2023-09-28 11:56:10 -0400

    Updated packages and removed references to yarn.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import Application from "./Application.svelte"
+import App from "./App.svelte"
 import "./index.global.css"
 
-export const application = new Application({
+export const app = new App({
   target: document.getElementById("root"),
 })
